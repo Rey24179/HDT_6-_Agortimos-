17a766c5cb51427b6f3fd6d23ce5e6d7ca023420
Rey24179 <rey24179@uvg.edu.gt>
2025-03-11 21:53:36 -0600

    Pruebas uniatarias

---
+++ PokemonManagerTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+public class PokemonManagerTest {
+    @Test
+    public void testAddPokemonToUser() {
+
+    }
+
+    @Test
+    public void testLoadFromFile() {
+
+    }
+
+    @Test
+    public void testShowAllPokemonSortedByType() {
+
+    }
+
+    @Test
+    public void testShowPokemonByAbility() {
+
+    }
+
+    @Test
+    public void testShowPokemonData() {
+
+    }
+
+    @Test
+    public void testShowUserCollectionSortedByType() {
+
+    }
+}
